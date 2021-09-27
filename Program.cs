4ea0d5354f74d473a7d3200bf0559c9172478a05
Francis Chung <francis.chung@gmail.com>
2021-09-27 13:06:57 -0700

    add loops

--- Program.cs
+++ Program.cs
@@ -3,10 +3,40 @@
 namespace firstcsharp
 {
     class Program
-    {
+    { 
+         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        int i = 1;
+
+        for(i = 1; i <=255; i++){
+         Console.WriteLine(i);
+        }
+        for(i = 1; i < 100; i++){
+            if (i % 15 == 0){
+                i++;
+            }
+            if (i % 3 == 0){
+                Console.WriteLine(i);
+            }
+            if (i % 5 == 0){
+                Console.WriteLine(i);
+            } 
+        }
+                for(i = 1; i < 100; i++){
+            if (i % 15 == 0){
+                Console.WriteLine("Fizzbuzz");
+            }
+            if (i % 3 == 0){
+                Console.WriteLine("Fizz");
+            }
+            if (i % 5 == 0){
+                Console.WriteLine("Buzz");
+            } 
+        }
+
+
+        }  
         }
     }
-}
+
